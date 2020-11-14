bd0704d36e251d599230413f22be1c24d91fe0ff
Surajit Barman <Surajit.Barman@cognizant.com>
2020-11-14 08:37:43 -0600

    Added Logger

--- src/main/java/mn/sample/application/controller/PersonController.java
+++ src/main/java/mn/sample/application/controller/PersonController.java
@@ -4,6 +4,8 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import mn.sample.application.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -12,10 +14,13 @@ import java.util.Optional;
 
 @Controller("/persons")
 public class PersonController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
+
     List<Person> persons = new ArrayList<>();
 
     @Post
     public Person add(Person person) {
+        LOGGER.info("Adding a person");
         person.setId(persons.size() + 1);
         persons.add(person);
         return person;
@@ -23,6 +28,7 @@ public class PersonController {
 
     @Get("/{id:4}")
     public Optional<Person> findById(@NotNull Integer id) {
+        LOGGER.info("Finding a person {}", id);
         return persons.stream()
                 .filter(it -> it.getId().equals(id))
                 .findFirst();
@@ -30,6 +36,7 @@ public class PersonController {
 
     @Get
     public List<Person> findAll() {
+        LOGGER.info("Returning all persons");
         return persons;
     }
 }
